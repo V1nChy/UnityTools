a43fa261b7db7f8467d2153e7e85a981e96d9a9f
V1nChy <593386778@qq.com>
2019-11-29 11:37:15 +0800

    scene window

---
+++ EditorTest/Assets/SceneEditWindow/Editor/GUIStyleViewer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GUIStyleViewer : EditorWindow
+{
+
+    Vector2 scrollPosition = new Vector2(0, 0);
+    string search = "";
+    GUIStyle textStyle;
+
+
+    private static GUIStyleViewer window;
+    [MenuItem("Tools/GUIStyleViewer", false, 100)]
+    private static void OpenStyleViewer()
+    {
+        window = GetWindow<GUIStyleViewer>(false, "查看内置GUIStyle");
+    }
+
+    void OnGUI()
+    {
+        if (textStyle == null)
+        {
+            textStyle = new GUIStyle("WhiteBoldLabel");
+            textStyle.fontSize = 20;
+        }
+
+        GUILayout.BeginHorizontal("HelpBox");
+        GUILayout.Label("点击示例，可以将其名字复制下来", textStyle);
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("Search:");
+        search = EditorGUILayout.TextField(search);
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal("PopupCurveSwatchBackground");
+        GUILayout.Label("示例", textStyle, GUILayout.Width(300));
+        GUILayout.Label("名字", textStyle, GUILayout.Width(300));
+        GUILayout.EndHorizontal();
+
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+        foreach (var style in GUI.skin.customStyles)
+        {
+            if (style.name.ToLower().Contains(search.ToLower()))
+            {
+                GUILayout.Space(15);
+                GUILayout.BeginHorizontal("PopupCurveSwatchBackground");
+                if (GUILayout.Button(style.name, style, GUILayout.Width(300)))
+                {
+                    EditorGUIUtility.systemCopyBuffer = style.name;
+                    Debug.LogError(style.name);
+                }
+                EditorGUILayout.SelectableLabel(style.name, GUILayout.Width(300));
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        GUILayout.EndScrollView();
+    }
+}
---
+++ EditorTest/Assets/SceneEditWindow/Editor/SampleSceneEditWindow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+public class SampleSceneEditWindow
+{
+    private static Rect _windowSize = new Rect(8, 24, 300, 100);
+    private static bool _enabled = false;
+    private static Color matColor;
+    // 加载脚本时调用
+    [InitializeOnLoadMethod]
+    private static void SampleSceneEdit()
+    {
+        SceneView.onSceneGUIDelegate += OnSceneGUI;
+    }
+    private static void OnSceneGUI(SceneView sceneView)
+    {
+        if (!GetIsTargetScene())
+        {
+            return;
+        }
+        GUILayout.Window(1, _windowSize, DrawWindow, "Sample Window");
+    }
+    public static void DrawWindow(int id)
+    {
+        GUIStyle textStyle = new GUIStyle(GUI.skin.label);
+        textStyle.richText = true;
+        EditorGUILayout.LabelField("<color=white>Sample</color>", textStyle);
+    }
+    private static bool GetIsTargetScene()
+    {
+        return SceneManager.GetActiveScene().name.Contains("Sample_");
+    }
+}
